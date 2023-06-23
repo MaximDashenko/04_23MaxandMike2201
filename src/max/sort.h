561c9e7ff9f8365dd5e2205cca13e6888031a579
MaximDashenko <134542501+MaximDashenko@users.noreply.github.com>
2023-06-23 18:15:47 +0300

    radix (#4)

--- src/max/sort.h
+++ src/max/sort.h
@@ -10,14 +10,61 @@
 
 namespace max {
 
-    template<typename T, int i>
-    void sort(T (&array)[i], Comparator<T> comparator) {
-
+    // default implementation for all types
+    // bumble-sort
+    template<typename T, int size>
+    void sort(T (&array)[size], Comparator<T> comparator) {
+        for (int pass = 0; pass < size - 1; ++pass) {
+            auto swapped = false;
+            for (int j = 0; j < size - pass - 1; ++j) {
+                if (comparator(array[j], array[j + 1]) > 0) {
+                    std::swap(array[j], array[j + 1]);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
     }
 
-    template<typename T, int i>
-    void sort(T (&array)[i]) {
-        sort<T, i>(array, default_comparator);
+    template<typename T, int size>
+    void sort(T (&array)[size]) {
+        sort<T, size>(array, default_comparator);
+    }
+
+
+    // specialized template for unsigned int
+    // radix-sort
+    template<int size>
+    void sort(unsigned int (&array)[size]) {
+        int max = array[0];
+        for (int i = 1; i < size; ++i) {
+            if (max < i) {
+                max = i;
+            }
+        }
+        for (int div = 1; max / div > 0; div *= 10) {
+            int output[size];
+            int count[10] = {0};
+
+            for (int i = 0; i < size; i++) {
+                count[(array[i] / div) % 10]++;
+            }
+
+            for (int i = 1; i < 10; i++) {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = size - 1; i >= 0; i--) {
+                output[count[(array[i] / div) % 10] - 1] = array[i];
+                count[(array[i] / div) % 10]--;
+            }
+
+            for (int i = 0; i < size; i++) {
+                array[i] = output[i];
+            }
+        }
     }
 
 }
