6f18e6c6ed23319371599920c03c5cb798dd3296
Maxim Dashenko <maximdashenko15@gmail.com>
2023-06-23 21:03:29 +0300

    Added Misha cod

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "max/sort.h"
+#include "misha/sort.h"
 
 template<typename T, int size>
 void print_array(T (&array)[size]) {
@@ -12,8 +13,25 @@ void print_array(T (&array)[size]) {
 }
 
 int main() {
+    // max
     unsigned int array[] = {10, 3, 4, 3, 4, 9, 1, 0};
     max::sort(array);
     print_array(array);
+
+    double array2[] = {10.0, 3.0, 4.0, 3.0, 4.0, 9.0, 1.0, 0.0};
+    max::sort(array2);
+    print_array(array2);
+
+    // misha
+    int array3[] = {10, 3, 4, 3, 4, 9, 1, 0};
+    int size3 = sizeof(array3) / sizeof(array3[0]);
+    misha::bubbleSort(array3, size3);
+    print_array(array2);
+
+    int array4[] = {10, 3, 4, 3, 4, 9, 1, 0};
+    int size4 = sizeof(array4) / sizeof(array4[0]);
+    misha::insertionSort(array4, size4);
+    print_array(array4);
+
     return 0;
 }
